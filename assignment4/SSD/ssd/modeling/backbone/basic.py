31320fcd12a3828e630c8b604c7e69f1a7ff36c4
Dawid Kalicki <dawod28@gmail.com>
2021-03-15 17:49:16 +0100

    4a

--- assignment4/SSD/ssd/modeling/backbone/basic.py
+++ assignment4/SSD/ssd/modeling/backbone/basic.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 
 
 class BasicModel(torch.nn.Module):
@@ -20,6 +21,61 @@ class BasicModel(torch.nn.Module):
         image_channels = cfg.MODEL.BACKBONE.INPUT_CHANNELS
         self.output_feature_shape = cfg.MODEL.PRIORS.FEATURE_MAPS
 
+        # parameters for maxpool layers
+        f_pool = 2
+        s_pool = 2
+
+
+        self.block1 = nn.Sequential(
+            nn.Conv2d(image_channels, 32, 3, 1, 1),
+            nn.MaxPool2d(f_pool, s_pool),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.MaxPool2d(f_pool, s_pool),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(64, self.output_channels[0], 3, 2, 1),
+            # ------------------------------------------------------
+        )
+        self.block2 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(self.output_channels[0], 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(128, self.output_channels[1], 3, 2, 1),
+            # ------------------------------------------------------
+        )
+        self.block3 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(self.output_channels[1], 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(256, self.output_channels[2], 3, 2, 1),
+            # ------------------------------------------------------
+        )
+        self.block4 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(self.output_channels[2], 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(128, self.output_channels[3], 3, 2, 1),
+            # ------------------------------------------------------
+        )
+        self.block5 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(self.output_channels[3], 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(128, self.output_channels[4], 3, 2, 1),
+        )
+            # --------------------------------------------------------
+        self.block6 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(self.output_channels[4], 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(128, self.output_channels[5], 3, 2, 0),
+            # --------------------------------------------------------
+        )
+
     def forward(self, x):
         """
         The forward functiom should output features with shape:
@@ -33,9 +89,25 @@ class BasicModel(torch.nn.Module):
         where out_features[0] should have the shape:
             shape(-1, output_channels[0], 38, 38),
         """
+
         out_features = []
+
+        x = self.block1(x)
+        out_features.append(x)
+        x = self.block2(x)
+        out_features.append(x)
+        x = self.block3(x)
+        out_features.append(x)
+        x = self.block4(x)
+        out_features.append(x)
+        x = self.block5(x)
+        out_features.append(x)
+        x = self.block6(x)
+        out_features.append(x)
+
         for idx, feature in enumerate(out_features):
             w, h = self.output_feature_shape[idx]
+            out_channel = self.output_channels[idx]
             expected_shape = (out_channel, h, w)
             assert feature.shape[1:] == expected_shape, \
                 f"Expected shape: {expected_shape}, got: {feature.shape[1:]} at output IDX: {idx}"
