0d52a0897dbca8ab0690f81ed4323d9f87ecca35
Dawid Kalicki <dawod28@gmail.com>
2021-03-28 18:16:25 +0200

    files for ed_video

---
+++ ed_video/plot_layer.py
@@ -0,0 +1,95 @@
+
+import matplotlib.pyplot as plt
+from PIL import Image
+import torchvision
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+def plot_layer(model, layer, channels, image_path):
+
+  image = Image.open(image_path)
+  print("Image shape:", image.size)
+
+  #model = torchvision.models.resnet18(pretrained=True)
+
+  model_layers = model.feature_extractor.children()
+  model_layers = [l for l in model_layers]
+  #layers.pop()
+  #layers.pop()
+
+  #print(layers)
+  #print(len(layers))
+
+  #first_conv_layer = model.conv1
+  #print("First conv layer weight shape:", first_conv_layer.weight.shape)
+  #print("First conv layer:", first_conv_layer)
+
+  # Resize, and normalize the image with the mean and standard deviation
+  image_transform = torchvision.transforms.Compose([
+      torchvision.transforms.Resize((224, 224)),
+      torchvision.transforms.ToTensor(),
+      #torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+  ])
+  image = image_transform(image)[None]
+  print("Image shape:", image.shape)
+
+  activation = get_activations(image, layer, model_layers)
+
+  #activation = first_conv_layer(image)
+  print("Activation shape:", activation.shape)
+
+  #indices = [14, 26, 32, 49, 52]
+  #indices = range(10)
+
+  fig = plt.figure(1,(10,10))
+  grid = ImageGrid(fig, 111,
+                   nrows_ncols=(1,len(channels)),
+                   axes_pad=0.1,)
+
+  for n, i in enumerate(channels, start=0):
+    img = torch_image_to_numpy(activation.data[0,i,:,:])
+    img = img.squeeze()
+    grid[n].imshow(img,cmap='gray',interpolation='none')
+
+  #for n, i in enumerate(indices, start=0):
+  #  img = torch_image_to_numpy(first_conv_layer.weight.data[i,:,:,:])
+  #  img = cv2.resize(img, dsize=(112,112), interpolation=cv2.INTER_NEAREST)
+  #  img = img.squeeze()
+  #  grid[n].imshow(img,interpolation='none')
+
+
+  fig.savefig("layer_ouputs.png")
+
+
+def get_activations(in_, layer, model_layers):
+  try:
+    out = model_layers[layer](in_)
+    return get_activations(out, layer+1, model_layers)
+  except:
+    return out
+
+
+def torch_image_to_numpy(image: torch.Tensor):
+    """
+    Function to transform a pytorch tensor to numpy image
+    Args:
+        image: shape=[3, height, width]
+    Returns:
+        image: shape=[height, width, 3] in the range [0, 1]
+    """
+    # Normalize to [0 - 1.0]
+    image = image.detach().cpu() # Transform image to CPU memory (if on GPU VRAM)
+    image = image - image.min()
+    image = image / image.max()
+    image = image.numpy()
+    if len(image.shape) == 2: # Grayscale image, can just return
+        return image
+    assert image.shape[0] == 3, "Expected color channel to be on first axis. Got: {}".format(image.shape)
+    image = np.moveaxis(image, 0, 2)
+    return image
+
+
+
---
+++ ed_video/test.py
@@ -0,0 +1,142 @@
+import pathlib
+import matplotlib.pyplot as plt
+import utils
+from torch import nn
+
+from dataloaders import load_mnist
+from trainer import Trainer, compute_loss_and_accuracy
+from plot_layer import plot_layer
+
+
+class Model1(nn.Module):
+
+    def __init__(self,
+                 image_channels,
+                 num_classes):
+        """
+            Is called when model is initialized.
+            Args:
+                image_channels. Number of color channels in image (1)
+                num_classes: Number of classes we want to predict (10)
+        """
+        super().__init__()
+
+        # conv layer parameters
+        f=3
+        s=1
+        p=1
+
+        # max pool parameters
+        s_pool = 2
+        f_pool = 2
+
+        num_filters1 = 128
+        num_filters2 = 128
+        num_filters3 = 128
+        num_filters4 = 128
+        num_filters5 = 128
+        num_filters6 = 128
+
+        self.num_classes = num_classes
+        # Define the convolutional layers
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(image_channels, num_filters1, f, s, p),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_filters1),
+
+            nn.Conv2d(num_filters1, num_filters2, f, s, p),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_filters2),
+            nn.MaxPool2d(f_pool, s_pool),
+
+            nn.Conv2d(num_filters2, num_filters3, f, s, p),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_filters3),
+
+            nn.Conv2d(num_filters3, num_filters4, f, s, p),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_filters4),
+            nn.MaxPool2d(f_pool, s_pool),   
+
+            nn.Conv2d(num_filters4, num_filters5, f, s, p),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_filters5),
+            
+            nn.Conv2d(num_filters5, num_filters6, f, s, p),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_filters6),
+        )
+        
+
+        # The output of feature_extractor will be [batch_size, num_filters, 16, 16]
+        self.num_output_features = 8*8*num_filters6
+        num_nodes1 = 256
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.BatchNorm1d(self.num_output_features),
+            nn.Linear(self.num_output_features, num_nodes1),
+            nn.ReLU(),
+            nn.BatchNorm1d(num_nodes1),
+            nn.Linear(num_nodes1, self.num_classes)
+        )
+
+    def forward(self, x):
+        """
+        Performs a forward pass through the model
+        Args:
+            x: Input image, shape: [batch_size, 3, 32, 32]
+        """
+        batch_size = x.shape[0]
+
+        x = self.feature_extractor(x)
+        x = self.classifier(x)
+        out = x
+        
+        expected_shape = (batch_size, self.num_classes)
+        assert out.shape == (batch_size, self.num_classes),\
+            f"Expected output of forward pass to be: {expected_shape}, but got: {out.shape}"
+        return out
+
+def create_plots(trainer: Trainer, name: str):
+    plot_path = pathlib.Path("plots")
+    plot_path.mkdir(exist_ok=True)
+    # Save plots and show them
+    plt.figure(figsize=(20, 8))
+    plt.subplot(1, 2, 1)
+    plt.title("Cross Entropy Loss")
+    utils.plot_loss(trainer.train_history["loss"], label="Training loss", npoints_to_average=10)
+    utils.plot_loss(trainer.validation_history["loss"], label="Validation loss")
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.title("Accuracy")
+    utils.plot_loss(trainer.validation_history["accuracy"], label="Validation Accuracy")
+    plt.legend()
+    plt.savefig(plot_path.joinpath(f"{name}_plot.png"))
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Set the random generator seed (parameters, shuffling etc).
+    utils.set_seed(1)
+    epochs = 20
+    batch_size = 64
+    learning_rate = 5e-2
+    early_stop_count = 4
+    dataloaders = load_mnist(batch_size)
+    model = Model1(image_channels=1, num_classes=10)
+    trainer = Trainer(
+        batch_size,
+        learning_rate,
+        early_stop_count,
+        epochs,
+        model,
+        dataloaders
+    )
+    train = True
+    if train == True:
+        trainer.train()
+        trainer.get_final_results()
+        create_plots(trainer, "test")
+    else:
+        trainer.get_final_results()
